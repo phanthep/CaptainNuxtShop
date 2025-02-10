3a69edbcfb91b5eb11c634d59da4770c2b0172c2
Mr.Phanthep Jitgaroon <phanthep.jitgaroon@gmail.com>
2025-02-10 15:52:01 +0700

    clone from github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,28 +5,32 @@ import { createResolver } from '@nuxt/kit'
 const { resolve } = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
-    css: [
-        'vuetify/lib/styles/main.sass',
-        '@mdi/font/css/materialdesignicons.min.css'
-    ],
-    build: {
-        transpile: ['vuetify'],
-    },    
-    /*hooks: {
-        'vite:extendConfig': (config) => {
-          config.plugins.push(
-            vuetify({
-              styles: { configFile: resolve('./settings.scss') },
-            })
-          )
-        }
-    },*/
-    vite: {
-        ssr:{
-            noExternal: ['vuetify']
-        },
-        define: {
-        'process.env.DEBUG': false,
-        },
-    }
+  css: [
+      'vuetify/lib/styles/main.sass',
+      '@mdi/font/css/materialdesignicons.min.css'
+  ],
+
+  build: {
+      transpile: ['vuetify'],
+  },
+
+  /*hooks: {
+      'vite:extendConfig': (config) => {
+        config.plugins.push(
+          vuetify({
+            styles: { configFile: resolve('./settings.scss') },
+          })
+        )
+      }
+  },*/
+  vite: {
+      ssr:{
+          noExternal: ['vuetify']
+      },
+      define: {
+      'process.env.DEBUG': false,
+      },
+  },
+
+  compatibilityDate: '2025-02-10'
 })
